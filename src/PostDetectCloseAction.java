7003a60a37797fc3d58ee2ffe17b1fd6b7cb1db5
Zhang-0952 <357068044@qq.com>
2022-11-17 18:20:18 +0800

    Demo建立

---
+++ src/PostDetectCloseAction.java
@@ -0,0 +1,15 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * @author: zx
+ * @date: 2022/10/29
+ *
+ */
+public class PostDetectCloseAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        JavaExecutePythonUtils.proc.destroyForcibly();
+    }
+}
